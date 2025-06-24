2746b6b1331cefc24353ad013e0fa705fb028739
Annavitoriads <annavitoriavianads@gmail.com>
2025-06-24 20:19:51 -0300

    Add files via upload

---
+++ Models/Ia/IaRequestModel.cs
@@ -0,0 +1,14 @@
+namespace IAChatMVC.Models.Ia
+{
+public class IaRequestModel
+{
+    public string? Input { get; set; }
+
+    public IFormFile? File { get; set; }
+
+    public string? TipoAcao { get; set; }
+
+    public string? Response { get; set; }
+}
+}
+
